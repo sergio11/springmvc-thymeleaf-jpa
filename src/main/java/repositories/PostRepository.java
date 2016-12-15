4752af23854039cff1f6b50ceb7a12fdcfd7c498
Sergio Sánchez <sss4esob@gmail.com>
2016-12-15 20:06:05 +0100

    refactor(frontend): only post published can be seen on frontend.

--- src/main/java/repositories/PostRepository.java
+++ src/main/java/repositories/PostRepository.java
@@ -4,15 +4,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import models.Post;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import projection.PostByAuthor;
 import projection.PostDetail;
 import projection.PostSummary;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query("SELECT p FROM Post p WHERE p.id = :id AND ( p.published = true OR p.author.username = :currentUser )")
-    PostDetail findByIdAndPublishedTrueOrUserIsAuthor(@Param("id") Long id, @Param("currentUser") String currentUser);
     PostDetail findByIdAndPublishedTrue(Long id);
     PostDetail findById(Long id);
     List<PostSummary> findFirst5ByPublishedTrueOrderByDateDesc();
