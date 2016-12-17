997ba17f651dd15dbfbc529b9245134b47fb2f3a
Sergio Sánchez <sss4esob@gmail.com>
2016-12-17 15:09:32 +0100

    fix(post): solve problems with update partials

--- src/main/java/controllers/admin/PostController.java
+++ src/main/java/controllers/admin/PostController.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import services.PostService;
@@ -40,9 +42,13 @@ import projection.PostByAuthor;
  */
 @Controller("AdminPostController")
 @RequestMapping("/admin/posts")
+@SessionAttributes({PostController.ATTRIBUTE_NAME})
 public class PostController {
     
     private static Logger logger = LoggerFactory.getLogger(PostController.class);
+    
+    public static final String ATTRIBUTE_NAME = "post";
+    public static final String BINDING_RESULT_NAME = "org.springframework.validation.BindingResult." + ATTRIBUTE_NAME;
     
     @Autowired
     private PostService postService;
@@ -59,11 +65,14 @@ public class PostController {
     
     @GetMapping("/edit/{postId}")
     public String showUpdatePostForm(@PathVariable Long postId, Model model) {
-        Post post = postService.findById(postId);
-        if (post == null) {
-            throw new PostNotFoundException();
+        /* if "fresh" GET (ie, not redirect w validation errors): */
+        if(!model.containsAttribute(BINDING_RESULT_NAME)) {
+            Post post = postService.findById(postId);
+            if (post == null) {
+                throw new PostNotFoundException();
+            }
+            model.addAttribute(ATTRIBUTE_NAME, post);
         }
-        model.addAttribute("post", post);
         return "admin/post/edit";
     }
     
@@ -88,19 +97,26 @@ public class PostController {
     
     @GetMapping("/create")
     public String showCreatePostForm(Model model){
-        model.addAttribute("post", new Post());
+        model.addAttribute(ATTRIBUTE_NAME,  new Post());
         return "admin/post/create";
     }
     
     @PostMapping("/save")
-    public String processPost(@RequestPart("postImage") MultipartFile postImage, @ModelAttribute @Valid Post post, Errors errors, 
-            @CurrentUserAttached User activeUser, RedirectAttributes model) throws IOException, SQLException{
+    public String processPost(
+            @RequestPart("postImage") MultipartFile postImage, 
+            @ModelAttribute(ATTRIBUTE_NAME) @Valid Post post, 
+            BindingResult bindingResult, 
+            @CurrentUserAttached User activeUser, 
+            RedirectAttributes model) throws IOException, SQLException{
         
-        if(errors.hasErrors()){
-            return "admin/post/create";
+        String url = "redirect:/admin/posts/all";
+
+        if (bindingResult.hasErrors()) {
+            model.addFlashAttribute(BINDING_RESULT_NAME, bindingResult);
+            return url;
         }
-        
-        if(postImage != null && !postImage.isEmpty()){
+
+        if (postImage != null && !postImage.isEmpty()) {
             logger.info("Añadiendo información de la imagen");
             FileImage image = new FileImage();
             image.setName(postImage.getName());
@@ -109,19 +125,17 @@ public class PostController {
             image.setContent(postImage.getBytes());
             post.setImage(image);
         }
-        
+
         post.setAuthor(activeUser);
-        if(post.getId() == null){
-            logger.info("Creando Nuevo Post");
+        if (post.getId() == null) {
             postService.create(post);
-        }else{
-            logger.info("Editando Post");
+        } else {
             postService.edit(post);
         }
-        
+
         List<String> successMessages = new ArrayList();
-        successMessages.add(messageSource.getMessage("message.post.save.success", new Object[] {post.getId()}, Locale.getDefault()));
-        model.addFlashAttribute("successFlashMessages", successMessages);        
-        return "redirect:/admin/posts/all";
+        successMessages.add(messageSource.getMessage("message.post.save.success", new Object[]{post.getId()}, Locale.getDefault()));
+        model.addFlashAttribute("successFlashMessages", successMessages);
+        return url;
     }
 }
